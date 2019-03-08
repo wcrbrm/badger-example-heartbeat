36f1c96033726cbe3a4a4ab5774fd8b2ccbd511d
wcrbrm <viclopata@gmail.com>
2019-03-08 23:16:18 +0200

    draft of the server and metrics

---
+++ server/dbclient.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+// Database Client structure
+type DbClient struct {
+	Dsn            string
+	DB             *sqlx.DB
+}
+
+// Connect - return a connection to a postgres database wi
+func NewDatabaseClient() *DbClient {
+	DSN, ok := os.LookupEnv("PGSQL_DSN")
+	if !ok {
+		DSN = "postgresql://postgres:postgres@localhost:5432/postgres?sslmode=disable"
+	}
+	db, err := sqlx.Connect("postgres", DSN)
+	if err != nil {
+		log.Fatal("[db] Fatal Error, cannot open postgres database ", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("[db] Ping Error", err)
+	}
+	return &DbClient{DSN, db}
+}
--- server/server.go
+++ server/server.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	_ "github.com/dgraph-io/badger"
-	_ "github.com/gin-gonic/gin"
-	_ "github.com/Depado/ginprom"
+	gin     "github.com/gin-gonic/gin"
+	ginprom "github.com/Depado/ginprom"
 	_ "github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Started")
+	fmt.Println("Starting")
+
+	r := gin.Default()
+	p := ginprom.New(
+		ginprom.Engine(r),
+		ginprom.Subsystem("gin"), 
+		ginprom.Path("/metrics"), 
+	)
+	r.Use(p.Instrument())
+
+	r.POST("/heartbeat", func(c *gin.Context) {})
+	r.GET("/active", func(c *gin.Context) {})
+        r.GET("/all",   func(c *gin.Context) {})
+        r.POST("/search",  func(c *gin.Context) {})
+	r.Run("0.0.0.0:8092")
 }
